e3a5f3a06ed97ffc271c225777066c4672dd9427
liuyibao <734988964@qq.com>
2018-04-17 20:49:52 +0800

    flag parse

--- walle.go
+++ walle.go
@@ -3,15 +3,33 @@ package main
 import (
 	"net/http"
 	"github.com/julienschmidt/httprouter"
+	"github.com/walle-go/walle-go/controller"
+	"flag"
 	"github.com/walle-go/walle-go/migration"
 	"github.com/walle-go/walle-go/repository"
-	"github.com/walle-go/walle-go/controller"
+	"log"
 )
 
 func main()  {
-	migration.Migration()
-	var userRepo repository.UserRepo
-	userRepo.InsertOne()
+	var isHelp,isInit,isTest bool
+	flag.BoolVar(&isHelp, "h", false, "this help")
+	flag.BoolVar(&isInit, "i", false, "init database")
+	flag.BoolVar(&isTest, "t", false, "test sql")
+	flag.Parse()
+
+	if isHelp {
+		flag.Usage()
+	} else if isInit {
+		initDatabase()
+	} else if isTest {
+		test()
+	} else {
+		serve()
+	}
+}
+
+func serve() {
+	log.Println("Start serve...")
 	mux := httprouter.New()
 	mux.GET("/user/:id", controller.UserList)
 	server := &http.Server{
@@ -19,4 +37,17 @@ func main()  {
 		Handler: mux,
 	}
 	server.ListenAndServe()
+}
+
+func initDatabase()  {
+	log.Println("Start database migration.")
+	migration.Migration()
+	log.Println("Migration end.")
+}
+
+func test() {
+	log.Println("Start insert into test data.")
+	var userRepo repository.UserRepo
+	userRepo.InsertOne()
+	log.Println("Insert into end.")
 }
